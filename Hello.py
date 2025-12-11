98ecc91dcd71557325691eee6a9d3c72328a72d8
Rohan8598 <rohanpatil7248@gmail.com>
2025-12-11 22:36:36 +0530

    Adding Geometry file

--- Hello.py
+++ Hello.py
@@ -1,4 +1,5 @@
 import Aritmatic
+from geometry import cal_rect_area, cal_circle_area     
 print("hello world")
 
 a=int(input("Enter first number: "))
@@ -7,3 +8,5 @@ b=int(input("Enter second number: "))
 
 Aritmatic.add(a, b)
 Aritmatic.subtract(a, b)
+cal_rect_area(a, b) 
+cal_circle_area(a, b)
---
+++ geometry.py
@@ -0,0 +1,5 @@
+def cal_rect_area(len,br):
+    print("rect area :",len * br)
+
+def cal_circle_area(len ,br):
+    print("circle area :",3.14 * (len * br))
